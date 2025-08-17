d2344752f2bd7d5ec757029762a77eb384dddd72
Anna Valerjevna Makarova <anmakaro@c1r9s3.42prague.com>
2025-08-17 16:49:44 +0200

    preEv

--- 00 - Starting/ex02/find_ft_type.py
+++ 00 - Starting/ex02/find_ft_type.py
@@ -1,17 +1,17 @@
 def all_thing_is_obj(object: any) -> int:
-    try:
-        if isinstance(object, list):
-            print(f"List : {type(object)}")
-        elif isinstance(object, tuple):
-            print(f"Tuple : {type(object)}")
-        elif isinstance(object, set):
-            print(f"Set : {type(object)}")
-        elif isinstance(object, dict):
-            print(f"Dict : {type(object)}")
-        elif isinstance(object, str):
-            print(f"{object} is in the kitchen : {type(object)}")
-        else:
-            print("Type not found")
-    except Exception:
+    obj_type = type(object)
+
+    if isinstance(object, list):
+        print(f"List : {obj_type}")
+    elif isinstance(object, tuple):
+        print(f"Tuple : {obj_type}")
+    elif isinstance(object, set):
+        print(f"Set : {obj_type}")
+    elif isinstance(object, dict):
+        print(f"Dict : {obj_type}")
+    elif isinstance(object, str):
+        print(f"{object} is in the kitchen : {obj_type}")
+    else:
         print("Type not found")
+
     return 42
--- 00 - Starting/ex02/tester.py
+++ 00 - Starting/ex02/tester.py
@@ -5,41 +5,10 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello": "titi!"}
 
-ft_str1 = "Brian"
-ft_str2 = "Toto"
-
-ft_int = 10
-ft_float = 3.14
-
-ft_none = None
-
-
-def ft_lambda(x):
-    return x
-
-
-def ft_func(x):
-    return x
-
-
-class MyClass:
-    pass
-
-
-ft_obj = MyClass()
-
-test_objects = [
-    ft_list, ft_tuple, ft_set, ft_dict,
-    ft_str1, ft_str2,
-    ft_int, ft_float,
-    ft_none,
-    ft_lambda, ft_func,
-    ft_obj
-]
-
-print("\n\033[38;5;219m=== BEGIN TEST ===\033[0m\n")
-for obj in test_objects:
-    print(f"\033[38;5;206mTesting object: \033[0m{repr(obj)}")
-    result = all_thing_is_obj(obj)
-    print(f"\033[38;5;118mReturned:\033[0m 🦎{result}🦎\n")
-print("\033[38;5;222m=== END TEST ===\033[0m\n")
+all_thing_is_obj(ft_list)
+all_thing_is_obj(ft_tuple)
+all_thing_is_obj(ft_set)
+all_thing_is_obj(ft_dict)
+all_thing_is_obj("Brian")
+all_thing_is_obj("Toto")
+print(all_thing_is_obj(10))
